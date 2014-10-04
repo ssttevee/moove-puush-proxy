8a36e46bba26e37bc4e9156992620dac9f774cd0
Steve Lam <stevyo99@yahoo.ca>
2014-10-04 05:24:37 -0700

    finished implementing invitation system

--- register.php
+++ register.php
@@ -16,28 +16,43 @@ if(!empty($_POST)) {
 		$file_db = new PDO(PDO_DATABASE_CONNECT);
 
 		/** Check for existing user **/
-		$sth = $file_db->prepare("select count(*) from users where email == ?");
+		$sth = $file_db->prepare("select password,id from users where email == ?");
 		$sth->execute(array($_POST["e"]));
-		$count = $sth->fetch(PDO::FETCH_NUM);
+		$res = $sth->fetchAll();
 
-		if ($count[0] > 0) {
+		if($_POST["i"] == "generate") {
+			if(hash("sha256", $_POST["p"]) == $res[0]["password"] && $res[0]["id"] == 1) {
+				/** Generate encryption key */
+				$index = str_split("0123456789abcdefghijklmnopqrstuvwxyz");
+				$inviteKey = "";
+				for ($i = 0; $i < 16; $i++)
+					$inviteKey .= $index[mt_rand(0, count($index) - 1)];
+				$res = $file_db->exec("insert into invites(code, time) VALUES (" . $file_db->quote($inviteKey) . ", " . $file_db->quote(0) . ")");
+				die($inviteKey);
+			} else {
+				die("unauthorized");
+			}
+		} else if (count($res) > 0) {
 			$file_db = null;
 			die("user exists");
 		}
 
-//		/** Check for valid invite **/
-//		$count = $file_db->query("select count(id) from invite where code = " . $file_db->quote($_POST["i"]) . "")->fetchColumn();
-//
-//		if ($count < 1) {
-//			$file_db = null;
-//			die("invalid invitation");
-//		}
+		/** Check for valid invite **/
+		$res = $file_db->query("select id from invites where code = " . $file_db->quote($_POST["i"]) . " and time = " . $file_db->quote(0))->fetchAll();
+
+		if (count($res) < 1) {
+			$file_db = null;
+			die("invalid invitation");
+		}
+
+		/** Use invitation code **/
+		$res = $file_db->exec("update invites set time = " . $file_db->quote(time()) . " where code = " . $file_db->quote($_POST["i"]));
 
 		/** Generate new API Key **/
 		$apiKey = strtoupper(hash("md5", time() . mt_rand() . $_SERVER['REMOTE_ADDR']));
 
 		/** INSERT new user **/
-		$count = $file_db->exec("insert into users(email, password, apikey) VALUES (" . $file_db->quote($_POST["e"]) . ", " . $file_db->quote(hash("sha256", $_POST["p"])) . ", " . $file_db->quote($apiKey) . ")");
+		$res = $file_db->exec("insert into users(email, password, apikey) VALUES (" . $file_db->quote($_POST["e"]) . ", " . $file_db->quote(hash("sha256", $_POST["p"])) . ", " . $file_db->quote($apiKey) . ")");
 
 		/** close the database connection **/
 		$file_db = null;
