540dcf05131d99a00617d553a3453f0709e2d544
Steve Lam <stevyo99@yahoo.ca>
2014-10-04 06:39:33 -0700

    changed dirs

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("DIR_DB", "../db/");
-define("DIR_STORAGE", "../storage/");
+define("DIR_DB", "db/");
+define("DIR_STORAGE", "storage/");
 define("ROOT_URL", "http://psh.ssttevee.com/");
 define("PDO_DATABASE_CONNECT", "sqlite:" . DIR_DB . "puush.sqlite3");
