3c3ed18db36e2386fad8a9367a0582859de0accd
Steve Lam <stevyo99@yahoo.ca>
2014-10-06 15:33:19 -0700

    fixed script blocking iv creation

--- lib/cipher.php
+++ lib/cipher.php
@@ -4,7 +4,7 @@ class Cipher {
 	private $securekey, $iv;
 	function __construct($textkey) {
 		$this->securekey = hash('sha256', $textkey, TRUE);
-		$this->iv = mcrypt_create_iv(32);
+		$this->iv = mcrypt_create_iv(32, MCRYPT_DEV_URANDOM);
 	}
 	function encrypt($input) {
 		return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->securekey, $input, MCRYPT_MODE_ECB, $this->iv));
