d90f108b2de92bde40ddfc1544f0d87bc5bb86ee
Steve Lam <stevyo99@yahoo.ca>
2014-10-09 15:37:27 -0700

    fixed

--- php/config.php
+++ php/config.php
@@ -3,4 +3,4 @@ define("DIR_DB", "../../");
 define("DIR_STORAGE", "../../storage/");
 define("DIR_THUMB_CACHE", "../../tcache/");
 define("ROOT_URL", "http://mv.ssttevee.com/");
-define("PDO_DATA_SOURCE_NAME", "sqlite:" . DIR_DB . "puush.sqlite3");
+define("PDO_DATA_SOURCE_NAME", "sqlite:" . DIR_DB . "moove.db");
